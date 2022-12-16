a46b635f47544b298f6f531569d89ef23ef9becc
DRG2Boss <113391397+DRG2Boss@users.noreply.github.com>
2022-12-16 16:54:18 -0500

    update

--- W7-whileMenuExample.py
+++ W7-whileMenuExample.py
@@ -6,12 +6,7 @@ for line in file_lines:
     split_line_list = line.split(',')
     country = {
         "name":split_line_list[0],
-        "pop":int(split_line_list[1]),
-        "change":float(split_line_list[2]),
-    }
-    country_data.insert(0, country)
-
-while True:
+        "pop":int(split_line_list[1]),while True:
     print("===========Please select and option below=======")
     print("[1] Find the country with the largest population")
     print("[2] Find the country with the smallest population")
@@ -45,3 +40,8 @@ while True:
         exit(0)
     else:
         print("invalid entry, please enter 1-4")
+        "change":float(split_line_list[2]),
+    }
+    country_data.insert(0, country)
+
+
